6a91811d14411f73051d0366320e4ae27a10cb63
Praid <105951840+Praidset@users.noreply.github.com>
2025-11-04 01:43:49 +0300

    int to IPv4

--- int to IPv4.py
+++ int to IPv4.py
@@ -1,4 +1,5 @@
-Take the following IPv4 address: 128.32.10.1
+
+'''Take the following IPv4 address: 128.32.10.1
 
 This address has 4 octets where each octet is a single byte (or 8 bits).
 
@@ -10,11 +11,8 @@ So 128.32.10.1 == 10000000.00100000.00001010.00000001
 
 Because the above IP address has 32 bits, we can represent it as the unsigned 32 bit number: 2149583361
 
-Complete the function that takes an unsigned 32 bit number and returns a string representation of its IPv4 address.
+Complete the function that takes an unsigned 32 bit number and returns a string representation of its IPv4 address.'''
 
-
-
-Решение:
 
 def dec_to_bin(dec):
     '''Decimal to binary conversion'''
